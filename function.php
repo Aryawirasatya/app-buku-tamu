6e1d54116f85f4610506974f36edd61c37b27999
arya wirasatya <aryawirasatya39@gmail.com>
2024-10-25 11:26:16 +0700

    menambahkan fitur edit data user

--- function.php
+++ function.php
@@ -88,6 +88,25 @@ function tambah_user($data){
     return mysqli_affected_rows($koneksi);
 }
 
+function ubah_user($data)
+{
+    global $koneksi;
+
+    $kode       = htmlspecialchars($data["id_user"]);
+    $username   = htmlspecialchars($data["username"]);
+    $user_role  = htmlspecialchars($data["user_role"]);
+
+    $query = "UPDATE users SET
+                username    = '$username',
+                user_role   = '$user_role'
+              WHERE id_user = '$kode'";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
+
+
 
 // -- // // panggil file koneksi php
 // -- // require_once('koneksi.php');
